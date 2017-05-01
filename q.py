df8d5cc1377b124ba7603520a667c9bb2578d422
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2017-05-01 19:59:51 -0300

    Adds plot capabilities and perimeter report.

--- q.py
+++ q.py
@@ -32,6 +32,8 @@ Use example:
 
 import argparse
 
+import matplotlib.pyplot as plt
+
 from solar import SolarSystem
 from forecast import Forecast
 
@@ -59,12 +61,47 @@ parser.add_argument(
     help='How many years to calculate.'
 )
 
+parser.add_argument(
+    '--plot-all', dest='plot_all', action='store_true', default=False,
+    help='Plot all dates to an snapshot with forecast name.'
+)
+
+parser.add_argument(
+    '--plot-predicted', dest='plot_predicted', action='store_true',
+    default=False, help='Plot only predicted weather to an snapshot with '
+    'forecast name.'
+)
+
+
+def plot(day, prediction, coords):
+    x = [c.delta_to_xy()[0] for c in coords]
+    y = [c.delta_to_xy()[1] for c in coords]
+
+    plt.clf()
+    plt.axis([-2500, 2500, -2500, 2500])
+    fig = plt.gcf()
+    ax = fig.gca()
+    circle1 = plt.Circle((0, 0), radius=500, color='b', fill=False)
+    circle2 = plt.Circle((0, 0), radius=1000, color='b', fill=False)
+    circle3 = plt.Circle((0, 0), radius=2000, color='b', fill=False)
+    ax.add_artist(circle1)
+    ax.add_artist(circle2)
+    ax.add_artist(circle3)
+
+    plt.scatter([0], [0])
+    plt.plot(x, y)
+    plt.title(prediction)
+    plt.savefig('plot/%d-%s.png' % (day, prediction))
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.days == 0 and args.years == 0:
         raise ValueError("An amount of days or years must be indicated.")
+
+    if args.plot_all and args.plot_predicted:
+        args.plot_predicted = False
 
     planet = solar.planet_by_name(args.from_planet)
     if planet is None:
@@ -79,13 +116,13 @@ if __name__ == '__main__':
     ))
 
     fc = Forecast()
-    totalized = {'total': total_days}
+    totalized = {'total': total_days, 'max_rain': {'day': None, 'perimeter': 0}}
 
     for day in range(total_days):
         coords = solar.positions(day)
         prediction = fc.predict(coords)
-        totalized.setdefault(prediction, 0)
-        totalized[prediction] += 1
+        totalized.setdefault(prediction['name'], 0)
+        totalized[prediction['name']] += 1
 
         if prediction is not None:
             print({
@@ -93,6 +130,17 @@ if __name__ == '__main__':
                 'coords': coords,
                 'forecast': prediction,
             })
+
+            if args.plot_predicted:
+                plot(day, prediction, coords)
+
+        if args.plot_all:
+            plot(day, prediction, coords)
+
+        if 'perimeter' in prediction and \
+                prediction['perimeter'] > totalized['max_rain']['perimeter']:
+            totalized['max_rain']['perimeter'] = prediction['perimeter']
+            totalized['max_rain']['day'] = day
 
     print("Totalization: %s" % totalized)
 
